36432689dfe9b16be3dd5a17ef5fa4c1453e50d0
Hideokun13 <77058528+Hideokun13@users.noreply.github.com>
2023-02-27 21:49:52 +0700

    added several file

---
+++ postgres_query.sql
@@ -0,0 +1,5 @@
+INSERT INTO pageview_counts VALUES ('Apple', 67, '2023-02-27T11:00:00+00:00');
+INSERT INTO pageview_counts VALUES ('Facebook', 1141, '2023-02-27T11:00:00+00:00');
+INSERT INTO pageview_counts VALUES ('Google', 994, '2023-02-27T11:00:00+00:00');
+INSERT INTO pageview_counts VALUES ('Amazon', 50, '2023-02-27T11:00:00+00:00');
+INSERT INTO pageview_counts VALUES ('Microsoft', 361, '2023-02-27T11:00:00+00:00');
